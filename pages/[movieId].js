cf52487382af511da9c3dd7830e18dc3f33369ee
Lewis Rotchell <rotchell92@gmail.com>
2021-06-27 20:00:46 +0100

    Working on movie page

--- pages/[movieId].js
+++ pages/[movieId].js
@@ -1,22 +1,46 @@
 import React from "react";
 import { getMovieById, getMovies } from "../helpers/api-util";
+import classes from "../styles/MoviePage.module.scss";
 
 const MovieDetailPage = ({ selectedMovie }) => {
-  const { title, description, genres, director, releaseDate, runTime, cast } =
-    selectedMovie;
+  const {
+    title,
+    backdrop_path,
+    poster_path,
+    overview,
+    genres,
+    director,
+    releaseDate,
+    runTime,
+    cast,
+  } = selectedMovie;
   console.log(selectedMovie);
   return (
-    <section>
-      {/* <header></header>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        <li>genres mapped here</li>
-      </ul>
-      <p>Directored By </p>
-      <p> Release Date: {releaseDate}</p>
-      <p> Run Time: {runTime}</p>
-      <p>Cast</p> */}
+    <section className={classes.MovieDetailPage}>
+      <header>
+        <img
+          className={classes.movieBackdrop}
+          src={`https://image.tmdb.org/t/p/w780/${backdrop_path}`}
+          alt=""
+        />
+      </header>
+
+      <div className={`section-padding ${classes.movieDetailsContainer}`}>
+        <img
+          className={classes.moviePoster}
+          src={`https://image.tmdb.org/t/p/w92/${poster_path}`}
+          alt=""
+        />
+        <h1>{title}</h1>
+        <p>{overview}</p>
+        <ul>
+          <li>genres mapped here</li>
+        </ul>
+        <p>Directored By </p>
+        <p> Release Date: {releaseDate}</p>
+        <p> Run Time: {runTime}</p>
+        <p>Cast</p>
+      </div>
     </section>
   );
 };
@@ -48,7 +72,7 @@ export async function getStaticPaths() {
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: "blocking",
   };
 }
 
