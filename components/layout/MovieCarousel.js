4107623064dcf1b971e1b55f6e4cd47e99e21f1d
Lewis Rotchell <rotchell92@gmail.com>
2021-06-28 15:29:04 +0100

    Added responsiveness to home screen

--- components/layout/MovieCarousel.js
+++ components/layout/MovieCarousel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import classes from "./MovieCarousel.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 // import "swiper/swiper-bundle.css";
@@ -14,12 +15,13 @@ import SwiperCore, {
 const NewReleases = ({ movies, title }) => {
   console.log(movies);
   return (
-    <section className={`section ${classes.newReleases}`}>
+    <section className={`section ${classes.carousel}`}>
       <h2>{title}</h2>
       <Swiper
         className={classes.swiperContainer}
         spaceBetween={14}
-        slidesPerView={3.3}
+        slidesPerView={"auto"}
+
         // navigation
         // pagination={{ clickable: true }}
         // scrollbar={{ draggable: true }}
@@ -27,10 +29,22 @@ const NewReleases = ({ movies, title }) => {
         // onSlideChange={() => console.log("slide change")}
       >
         {movies.map((movie) => (
-          <SwiperSlide key={movie.id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
-            ></img>
+          <SwiperSlide className={classes.swiperSlide} key={movie.id}>
+            {/* <img src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`} /> */}
+            <picture>
+              <source
+                media="(max-width: 799px)"
+                srcSet={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
+              />
+              <source
+                media="(min-width: 800px)"
+                srcSet={`https://image.tmdb.org/t/p/w154/${movie.poster_path}`}
+              />
+              <img
+                src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
+                alt=""
+              />
+            </picture>
           </SwiperSlide>
         ))}
       </Swiper>
