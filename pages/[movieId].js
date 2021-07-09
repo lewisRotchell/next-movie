69fa86375ed14c7821cd9c796c448daff8ab352a
Lewis Rotchell <rotchell92@gmail.com>
2021-07-09 08:57:44 +0100

    refactored

--- pages/[movieId].js
+++ pages/[movieId].js
@@ -112,19 +112,12 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  let newReleasesUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-GB&page=1&region=GB`;
-  let popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-GB&page=1&region=GB`;
-  let topRatedMoviesUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-GB&page=1&region=GB`;
-  const urls = [newReleasesUrl, popularMoviesUrl, topRatedMoviesUrl];
-
-  let data = await Promise.all(
-    urls.map((url) => fetch(url).then((response) => response.json()))
-  ).catch((error) => console.log(error));
+  const movies = await getMovies();
 
   const bigMovieArray = [
-    ...data[0].results,
-    ...data[1].results,
-    ...data[2].results,
+    ...movies[0].results,
+    ...movies[1].results,
+    ...movies[2].results,
   ];
 
   const paths = bigMovieArray.map((movie) => ({
